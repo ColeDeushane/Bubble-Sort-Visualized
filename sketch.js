859bbfb8143ff18ae3afa55a5ab6e4894e8d22f1
Cole Deushane <coledeushane@gmail.com>
2019-11-27 17:23:07 -0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,29 +1,80 @@
+/*
+ * Cole Deushane
+ * 27 November 2019
+ * P5JS Bubble Sort Visual
+*/
 
-var myLine = [];
-var x1;
-var y1;
-var x2;
-var y2;
+var heights = [];
+var x;
+var y;
+var temp;
+var sorted;
+var sort_start;
 
 function setup() {
-    createCanvas(800, 400);
-    background('grey');
 
-    x1 = 800;
-    y1 = 0;
-    x2 = x1;
-    y2 = 400;
+    createCanvas(800, 500);
+    x = 0;
+    y = 0;
+    sorted = false;
+    sort_start = false;
 
-    for(var i = 0; i < 800; i++){
-        myLine[i] = new Arrow(x1, y1, x2, y2);
+    // initializes the heights of the lines
+    for(var i = 0; i < width / 8; i++){
+        heights[i] = random(height);
+    }
+
+    // console.log("Number of Lines: " + heights.length);
+
+    }
+
+function draw() {
+    background(51);
+
+    if(sort_start) {
+        if(heights[y] < heights[y + 1]) {
+            temp = heights[y];
+            heights[y] = heights[y + 1];
+            heights[y + 1] = temp;
+        }
+
+        y++;
+
+        if(y >= heights.length - x - 1) {
+            y = 0;
+            x++;
+        }
+
+        if(x > heights.length){
+            //console.log("Sorting Complete.")
+            sorted = true;
+        }
+
+        for(var i = 0; i < heights.length; i++) {
+            strokeWeight(7);
+            if(heights[y] == heights[i] && !sorted) {
+                stroke('red');
+                line(i * 8, heights[i], i * 8, height);
+            } else if(sorted) {
+                stroke('green');
+                line(i * 8, heights[i], i * 8, height);
+            } else {
+                stroke(255);
+                line(i * 8, heights[i], i * 8, height);
+            }
+        }
+    } else {
+        for(var i = 0; i < heights.length; i++) {
+            strokeWeight(7);
+            stroke(255);
+            line(i * 8, heights[i], i * 8, height);
+        }
     }
 }
 
-
-function draw() {
-
-    for(var i = 0; i < myLine.length; i++) {
-        myLine[i].show();
+function keyPressed() {
+    if(key == ' ') {
+        //console.log("space");
+        sort_start = true;
     }
-
 }
